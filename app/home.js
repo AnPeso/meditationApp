ac3214f33f75495733c9d8c7ee7e143730eba41c
AnPeso <n2pean00@students.oamk.fi>
2025-02-25 08:30:49 -0500

    quotes

--- app/home.js
+++ app/home.js
@@ -6,6 +6,7 @@ import ScreenHeaderBtn from "../components/ScreenHeaderBtn";
 import Welcome from "../components/Welcome";
 import PopularMeditation from "../components/PopularMeditation";
 import DailyMeditation from "../components/DailyMeditation";
+import DailyQuote from "../components/DailyQuote";
 const Home = () => {
     const [userDetails, setUserDetails] = useState(null);
   useEffect(() => {
@@ -30,7 +31,8 @@ const Home = () => {
           testID="screensDisplay"
         >
 <Welcome userDetails={userDetails ? JSON.parse(userDetails) : null} />
-<PopularMeditation/>
+<DailyQuote/>
+<PopularMeditation />
 <DailyMeditation/>
 </View>
       </ScrollView>
